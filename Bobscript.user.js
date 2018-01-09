723c2eeaa1099161667af8faf315800d24900316
Jacob Alperin-Sheriff <jacobalperin-sheriff@jacob-alperin-sheriffs-macbook-pro.local>
2018-01-08 19:43:03 -0500

    more opt for Karen Veltri scripts

--- Bobscript.user.js
+++ Bobscript.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Bobscript
+// @name         Bobscript,
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Adds N/A to all form elements in the iframe on MTurk
+// @description  Speeds up Karen Veltri tasks by allowing efficient copy/paste from Linkedin
 // @author       You
 // @include        http://*.mturkcontent.com/*
 // @include        https://*.mturkcontent.com/*
@@ -12,6 +12,91 @@
 (function() {
     'use strict';
 
+    var current_paste_func=function(e) {
+        // cancel paste
+        e.preventDefault();
+        // get text representation of clipboard
+        var text = e.clipboardData.getData("text/plain");
+        var split_str=text.split("\n");
+        var my_re=/\d{4}/;
+        document.getElementById("current_firm").value=split_str[0];
+        document.getElementById("current_title").value=split_str[1];
+        document.getElementById("current_year").value=(my_re.exec(split_str[3]))[0];
+    };
+    var prev_paste_func=function(e) {
+        // cancel paste
+        e.preventDefault();
+        // get text representation of clipboard
+        var text = e.clipboardData.getData("text/plain");
+        var split_str=text.split("\n");
+        var last_val=this.id.substr(this.id.length-1);
+        console.log("last_val="+last_val);
+        var my_re=/\d{4}/;
+        document.getElementById("previous_co_"+last_val).value=split_str[0];
+        document.getElementById("previous_title_"+last_val).value=split_str[1];
+        document.getElementById("previous_year_"+last_val).value=(my_re.exec(split_str[3]))[0];
+    };
+
+    var law_paste_func=function(e) {
+        // cancel paste
+        e.preventDefault();
+        // get text representation of clipboard
+        var text = e.clipboardData.getData("text/plain");
+        var split_str=text.split("\n");
+        var last_val=this.id.substr(this.id.length-1);
+        console.log("last_val="+last_val);
+        var my_re=/\d{4}/;
+        var my_re2=/\d{4}/g;
+        var arr_pos = split_str.length -1;
+         var grad_year;
+         while(arr_pos >= 0)
+         {
+             grad_year=split_str[arr_pos].match(my_re2);
+             if(!(grad_year === null || grad_year.length>2))
+             {
+                 break;
+             }
+             arr_pos-=1;
+         }
+
+
+        document.getElementById("law_school").value=split_str[0];
+        document.getElementById("graduation_year").value=grad_year[grad_year.length-1];
+
+    };
+     var undergrad_paste_func=function(e) {
+        // cancel paste
+        e.preventDefault();
+        // get text representation of clipboard
+        var text = e.clipboardData.getData("text/plain");
+        var split_str=text.split("\n");
+        var last_val=this.id.substr(this.id.length-1);
+        console.log("last_val="+last_val);
+        var my_re=/\d{4}/;
+         var my_re2=/\d{4}/g;
+        var arr_pos = split_str.length -1;
+         var grad_year;
+         while(arr_pos >= 0)
+         {
+             grad_year=split_str[arr_pos].match(my_re2);
+             if(!(grad_year === null || grad_year.length>2))
+             {
+                 break;
+             }
+             arr_pos-=1;
+         }
+
+        document.getElementById("undergrad_school").value=split_str[0];
+        document.getElementById("undergrad_graduation_year").value=grad_year[grad_year.length-1];
+
+    };
+    document.getElementById("current_firm").addEventListener("paste",current_paste_func);
+    var i;
+    for(i=1; i<=3; i++) {
+        document.getElementById("previous_co_"+i).addEventListener("paste",prev_paste_func);
+    }
+    document.getElementById("law_school").addEventListener("paste",law_paste_func);
+    document.getElementById("undergrad_school").addEventListener("paste",undergrad_paste_func);
     var button = document.createElement("input");
     button.type="button";
     button.value="Fill N/A";
@@ -27,7 +112,10 @@
             //        console.log("the_inputs[\""+curr_var+"\"]="+the_inputs[curr_var]);
             if(the_inputs[i].value==="")
             {
-                the_inputs[i].value="N/A";
+                if(the_inputs[i].id != "web_url")
+                    the_inputs[i].value="N/A";
+                else
+                    the_inputs[i].value="http://NA.com";
             }
         }
     };
