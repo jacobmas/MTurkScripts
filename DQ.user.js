b8d98fe6c98b5d1342ff6a7fff00687bed5e2f3d
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-04-05 16:56:48 -0400

    DQ*

---
+++ DQ.user.js
@@ -0,0 +1,120 @@
+// ==UserScript==
+// @name         DQ
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Stuff about JJNissen
+// @author       You
+// @include        http://*.mturkcontent.com/*
+// @include        https://*.mturkcontent.com/*
+// @include        http://*.amazonaws.com/*
+// @include        https://*.amazonaws.com/*
+// @include https://worker.mturk.com/*
+// @include file://*
+// @grant  GM_getValue
+// @grant GM_setValue
+// @grant GM_addValueChangeListener
+// @grant        GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @connect google.com
+// @connect bing.com
+// @connect yellowpages.com
+// @connect *
+// @connect crunchbase.com
+
+// ==/UserScript==
+
+(function() {
+    function bad_website()
+    {
+        document.getElementById("websiteUnavailable").checked=true;
+    }
+
+
+    function init_DQ()
+    {
+        var url=document.getElementsByClassName("dont-break-out")[0].href;
+
+        console.log("url="+url);
+
+
+        GM_xmlhttpRequest({
+                method: 'GET',
+                url:    access_URI,
+                onerror: function(response) {
+                    bad_website();
+
+                },
+                onabort: function(response) {
+                    bad_website();
+                    return;
+
+
+
+                },
+                ontimeout: function(response) {
+                    bad_website();
+
+                },
+                onload: function(response) {}
+        });
+
+    }
+
+    /* Failsafe to stop it  */
+    window.addEventListener("keydown",function(e) {
+        if(e.key !== "F1") {
+            return;
+        }
+        GM_setValue("stop",true);
+     });
+
+
+    if (window.location.href.indexOf("mturkcontent.com") != -1 || window.location.href.indexOf("amazonaws.com") != -1)
+    {
+        var submitButton=document.getElementById("submitButton");
+        if(!submitButton.disabled )
+        {
+           
+            init_DQ();
+        }
+
+    }
+    else
+    {
+        //setTimeout(function() { btns_secondary[0].click(); }, 40000);
+        GM_setValue("returnHit",false);
+       GM_addValueChangeListener("returnHit", function() {
+                if(GM_getValue("returnHit")!==undefined && GM_getValue("returnHit")===true &&
+                  btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Return"
+                  )
+                {
+
+                  // btns_secondary[0].click();
+                }
+            });
+         /* Regular window at mturk */
+        var btns_primary=document.getElementsByClassName("btn-primary");
+        var btns_secondary=document.getElementsByClassName("btn-secondary");
+        if(GM_getValue("stop") !== undefined && GM_getValue("stop") === true)
+        {
+        }
+        else if(btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Skip" &&
+               btns_primary!==undefined && btns_primary.length>0 && btns_primary[0].innerText==="Accept")
+        {
+
+            /* Accept the HIT */
+          //btns_primary[0].click();
+        }
+        else
+        {
+            /* Wait to return the hit */
+            console.log("MOO");
+            var cboxdiv=document.getElementsByClassName("checkbox");
+            var cbox=cboxdiv[0].firstChild.firstChild;
+            if(cbox.checked===false) cbox.click();
+        }
+
+    }
+
+
+})();
