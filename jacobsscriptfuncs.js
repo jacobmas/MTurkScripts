2a6a2251f69650d0490714c2de929bf70cb227ca
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-07-22 15:23:11 -0400

    stuff

--- jacobsscriptfuncs.js
+++ jacobsscriptfuncs.js
@@ -1,4 +1,12 @@
-
+var state_map={"Alabama":"AL","Alaska":"AK","Arizona":"AZ","Arkansas":"AR","California":"CA","Colorado":"CO","Connecticut":"CT","Delaware":"DE",
+                   "District of Columbia": "DC", "Florida": "FL","Georgia": "GA", "Hawaii": "HI","Idaho":"ID","Illinois":"IL","Indiana":"IN","Iowa":"IA",
+                   "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD", "Massachusetts":"MA", "Michigan": "MI",
+                   "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV", "New Hampshire": "NH",
+                   "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH",
+                   "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD",
+                   "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+                   "Wisconsin": "WI", "Wyoming": "WY", "Ontario": "ON", "Quebec": "QC", "New Brunswick": "NB", "Alberta": "AB", "Saskatchewan": "SK",
+                   "Manitoba": "MB", "British Columbia": "BC","Nova Scotia": "NS"};
 function is_bad_url(the_url, bad_urls)
 {
     var i;
---
+++ template.js
@@ -0,0 +1,245 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  New script
+// @author       You
+// @include        http://*.mturkcontent.com/*
+// @include        https://*.mturkcontent.com/*
+// @include        http://*.amazonaws.com/*
+// @include        https://*.amazonaws.com/*
+// @include https://worker.mturk.com/*
+// @include file://*
+// @grant  GM_getValue
+// @grant GM_setValue
+// @grant GM_addValueChangeListener
+// @grant        GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @grant GM_openInTab
+// @connect google.com
+// @connect bing.com
+// @connect yellowpages.com
+// @connect *
+// @connect crunchbase.com
+// @require https://raw.githubusercontent.com/hassansin/parse-address/master/parse-address.min.js
+// @require https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/jacobsscriptfuncs.js
+// ==/UserScript==
+
+
+// VCF Do something with?
+(function() {
+    'use strict';
+
+    var automate=false;
+    var email_re = /(([^<>()\[\]\\.,;:\s@"：+=\/\?%]+(\.[^<>()\[\]\\.,;:：\s@"\?]+)*)|("[^\?]+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
+
+    var phone_re=/[\+]?[(]?[0-9]{3}[)]?[-\s\.\/]+[0-9]{3}[-\s\.\/]+[0-9]{4,6}/im;
+    var fax_re=/Fax[:]?[\s]?([\+]?[(]?[0-9]{3}[)]?[-\s\.\/]+[0-9]{3}[-\s\.\/]+[0-9]{4,6})/im;
+
+
+    var personal_email_domains=["aol.com","bigpond.com","frontiernet.net","gmail.com","icloud.com","mchsi.com","me.com","pacbell.net","rogers.com","rr.com","ymail.com"];
+    var my_query = {};
+    var email_list=[];
+    var sch_name="School District Name", sch_domain="Domain of school district";
+    var bad_urls=[];
+    var country_domains=[".ar",".at",".au",".br",".ch",".cn",".de",".eu",".fr",".it",".jp",".ro",".ru",".se",".tw",".uk",".uy",".vn"];
+    var first_try=true;
+
+    function check_function()
+    {
+	return true;
+    }
+    function check_and_submit(check_function, automate)
+    {
+	console.log("in check");
+	if(!check_function())
+	{
+            GM_setValue("returnHit",true);
+            console.log("bad");
+            return;
+	}
+	console.log("Checking and submitting");
+
+
+	if(automate)
+	{
+            setTimeout(function() { document.getElementById("submitButton").click(); }, 0);
+	}
+    }
+    function is_bad_name(b_name)
+    {
+	return false;
+    }
+
+    function query_response(response,resolve,reject) {
+        var doc = new DOMParser()
+        .parseFromString(response.responseText, "text/html");
+        console.log("in query_response\n"+response.finalUrl);
+        var search, b_algo, i=0, inner_a;
+	var b_url="crunchbase.com", b_name, b_factrow, b_caption;
+        var b1_success=false, b_header_search;
+        try
+        {
+            search=doc.getElementById("b_content");
+            b_algo=search.getElementsByClassName("b_algo");
+
+            console.log("b_algo.length="+b_algo.length);
+            if(b_algo.length===0)
+            {
+                console.log("b_algo length=0");
+                GM_setValue("returnHit",true);
+                return;
+            }
+           
+            for(i=0; i < b_algo.length; i++)
+            {
+                b_name=b_algo[i].getElementsByTagName("a")[0].textContent;
+                b_url=b_algo[i].getElementsByTagName("a")[0].href;
+		b_caption=b_algo[i].getElementsByClassName("b_caption")[0].innerText;
+
+
+
+                if(!is_bad_url(b_url, bad_urls) && !is_bad_name(b_name))
+                {
+                    b1_success=true;
+		    break;
+
+                }
+                
+            }
+	    if(b1_success)
+	    {
+		/* Do shit */
+	    }
+           
+
+        }
+        catch(error)
+        {
+	    console.log("Error "+error);
+	    reject(error);
+            return;
+            
+            //reject(JSON.stringify({error: true, errorText: error}));
+        }
+	reject("Nothing found");
+//        GM_setValue("returnHit",true);
+        return;
+
+    }
+
+    function query_search(search_str, resolve,reject) {
+        console.log("Searching with bing for "+search_str);
+        var search_URIBing='https://www.bing.com/search?q='+encodeURIComponent(search_str)+"&first=1&rdr=1";
+        var domain_URL='https://www.google.com/search?q='+encodeURIComponent(search_str);//+" company");
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url:    search_URIBing,
+
+            onload: function(response) {
+             //   console.log("On load in crunch_response");
+            //    crunch_response(response, resolve, reject);
+             query_response(response, resolve, reject);
+            },
+            onerror: function(response) { reject("Fail"); },
+            ontimeout: function(response) { reject("Fail"); }
+
+
+            });
+    }
+
+    /* Following the finding the district stuff */
+    function query_promise_then(result) {
+
+
+    }
+
+
+
+
+
+    function init_Query()
+    {
+
+       var wT=document.getElementById("workContent").getElementsByTagName("table")[0];
+        my_query={name};
+
+
+        const queryPromise = new Promise((resolve, reject) => {
+            console.log("Beginning URL search");
+            query_search(resolve, reject);
+        });
+        queryPromise.then(query_promise_then
+        )
+        .catch(function(val) {
+           console.log("Failed at this queryPromise " + val); GM_setValue("returnHit",true); });
+
+
+
+
+
+    }
+
+    /* Failsafe to stop it  */
+    window.addEventListener("keydown",function(e) {
+        if(e.key !== "F1") {
+            return;
+        }
+        GM_setValue("stop",true);
+     });
+
+
+    if (window.location.href.indexOf("mturkcontent.com") !== -1 || window.location.href.indexOf("amazonaws.com") !== -1)
+    {
+        var submitButton=document.getElementById("submitButton");
+        if(!submitButton.disabled )
+        {
+
+            init_Query();
+        }
+
+    }
+    else if(window.location.href.indexOf("mturk.com")!==-1)
+    {
+	/* Should be MTurk itself */
+       
+        if(automate)
+        {
+            setTimeout(function() { btns_secondary[0].click(); }, 20000); }
+        GM_setValue("returnHit",false);
+       GM_addValueChangeListener("returnHit", function() {
+                if(GM_getValue("returnHit")!==undefined && GM_getValue("returnHit")===true &&
+                  btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Return"
+                  )
+                {
+                    if(automate) {
+                        setTimeout(function() { btns_secondary[0].click(); }, 0); }
+                }
+            });
+         /* Regular window at mturk */
+        var btns_primary=document.getElementsByClassName("btn-primary");
+        var btns_secondary=document.getElementsByClassName("btn-secondary");
+        if(GM_getValue("stop") !== undefined && GM_getValue("stop") === true)
+        {
+        }
+        else if(btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Skip" &&
+               btns_primary!==undefined && btns_primary.length>0 && btns_primary[0].innerText==="Accept")
+        {
+
+            /* Accept the HIT */
+            if(automate) {
+                btns_primary[0].click(); }
+        }
+        else
+        {
+            /* Wait to return the hit */
+            console.log("MOO");
+            var cboxdiv=document.getElementsByClassName("checkbox");
+            var cbox=cboxdiv[0].firstChild.firstChild;
+            if(cbox.checked===false) cbox.click();
+        }
+
+    }
+
+
+})();
