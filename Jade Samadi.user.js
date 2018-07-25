d0ff12ddf71099f36d99155c6c7fa5f80206afd8
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-07-25 12:05:39 -0400

    stuff

---
+++ Jade Samadi.user.js
@@ -0,0 +1,188 @@
+// ==UserScript==
+// @name         Jade Samadi
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  New script
+// @author       You
+// @include        http://*.mturkcontent.com/*
+// @include        https://*.mturkcontent.com/*
+// @include        http://*.amazonaws.com/*
+// @include        https://*.amazonaws.com/*
+// @include https://worker.mturk.com/*
+// @include file://*
+// @grant  GM_getValue
+// @grant GM_setValue
+// @grant GM_addValueChangeListener
+// @grant        GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @grant GM_openInTab
+// @connect google.com
+// @connect bing.com
+// @connect yellowpages.com
+// @connect *
+// @connect crunchbase.com
+// @require https://raw.githubusercontent.com/hassansin/parse-address/master/parse-address.min.js
+// @require https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/jacobsscriptfuncs.js
+// ==/UserScript==
+
+
+// VCF Do something with?
+(function() {
+    'use strict';
+
+    var automate=true;
+    var email_re = /(([^<>()\[\]\\.,;:\s@"：+=\/\?%]+(\.[^<>()\[\]\\.,;:：\s@"\?]+)*)|("[^\?]+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
+
+    var phone_re=/[\+]?[(]?[0-9]{3}[)]?[-\s\.\/]+[0-9]{3}[-\s\.\/]+[0-9]{4,6}/im;
+    var fax_re=/Fax[:]?[\s]?([\+]?[(]?[0-9]{3}[)]?[-\s\.\/]+[0-9]{3}[-\s\.\/]+[0-9]{4,6})/im;
+
+
+    var personal_email_domains=["aol.com","bigpond.com","frontiernet.net","gmail.com","icloud.com","mchsi.com","me.com","pacbell.net","rogers.com","rr.com","ymail.com"];
+    var my_query = {};
+    var email_list=[];
+    var sch_name="School District Name", sch_domain="Domain of school district";
+    var bad_urls=[];
+    var country_domains=[".ar",".at",".au",".br",".ch",".cn",".de",".eu",".fr",".it",".jp",".ro",".ru",".se",".tw",".uk",".uy",".vn"];
+    var first_try=true;
+
+    function check_function()
+    {
+	return true;
+    }
+    function check_and_submit2(check_function,automate2)
+    {
+        console.log("in check");
+        if(!check_function())
+        {
+            GM_setValue("returnHit",true);
+            console.log("bad");
+            return;
+        }
+        console.log("Checking and submitting");
+
+
+        if(automate2)
+        {
+            setTimeout(function() { document.getElementById("submitButton").click(); }, 0);
+        }
+    }
+
+    function query_response(response,resolve,reject) {
+        var doc = new DOMParser()
+        .parseFromString(response.responseText, "text/html");
+        console.log("doc="+doc.body.innerText+"\n");
+        console.log("MOO2");
+        var emp_name_sec=doc.getElementById("employerNameSection");
+        var email_sec=doc.getElementById("emailSection");
+        var phone_sec=doc.getElementById("businessPhoneSection");
+
+        document.getElementsByName("companyName")[0].value=emp_name_sec.getElementsByClassName("public-register-data")[0].innerText;
+        if(email_sec!==null && email_sec!==undefined)
+        {
+            document.getElementsByName("EmailAddress")[0].value=email_sec.getElementsByClassName("public-register-data")[0].innerText;
+        }
+        else if(phone_sec!==null && phone_sec!==undefined)
+        {
+            document.getElementsByName("EmailAddress")[0].value=phone_sec.getElementsByClassName("public-register-data")[0].innerText;
+        }
+        check_and_submit2(check_function,true);
+
+
+    }
+   
+
+    /* Following the finding the district stuff */
+
+
+
+
+
+
+    function init_Query()
+    {
+
+       var url=document.getElementsByClassName("dont-break-out")[0].href;
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url:    url,
+
+            onload: function(response) {
+             //   console.log("On load in crunch_response");
+            //    crunch_response(response, resolve, reject);
+             query_response(response);
+            },
+            onerror: function(response) { console.log("FAil"); },
+            ontimeout: function(response) { console.log("Fail"); }
+
+
+            });
+
+
+
+
+    }
+
+    /* Failsafe to stop it  */
+    window.addEventListener("keydown",function(e) {
+        if(e.key !== "F1") {
+            return;
+        }
+        GM_setValue("stop",true);
+     });
+
+
+    if (window.location.href.indexOf("mturkcontent.com") != -1 || window.location.href.indexOf("amazonaws.com") != -1)
+    {
+        var submitButton=document.getElementById("submitButton");
+        if(!submitButton.disabled )
+        {
+
+            init_Query();
+        }
+
+    }
+    else
+    {
+	/* Should be MTurk itself */
+       
+        if(automate)
+        {
+            setTimeout(function() { btns_secondary[0].click(); }, 20000); }
+        GM_setValue("returnHit",false);
+       GM_addValueChangeListener("returnHit", function() {
+                if(GM_getValue("returnHit")!==undefined && GM_getValue("returnHit")===true &&
+                  btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Return"
+                  )
+                {
+                    if(automate) {
+                        setTimeout(function() { btns_secondary[0].click(); }, 0); }
+                }
+            });
+         /* Regular window at mturk */
+        var btns_primary=document.getElementsByClassName("btn-primary");
+        var btns_secondary=document.getElementsByClassName("btn-secondary");
+        if(GM_getValue("stop") !== undefined && GM_getValue("stop") === true)
+        {
+        }
+        else if(btns_secondary!==undefined && btns_secondary.length>0 && btns_secondary[0].innerText==="Skip" &&
+               btns_primary!==undefined && btns_primary.length>0 && btns_primary[0].innerText==="Accept")
+        {
+
+            /* Accept the HIT */
+            if(automate) {
+                btns_primary[0].click(); }
+        }
+        else
+        {
+            /* Wait to return the hit */
+            console.log("MOO");
+            var cboxdiv=document.getElementsByClassName("checkbox");
+            var cbox=cboxdiv[0].firstChild.firstChild;
+            if(cbox.checked===false) cbox.click();
+        }
+
+    }
+
+
+})();
