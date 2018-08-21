c83a75144861ea7124e34958c2fa1e7066a0eff8
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-08-21 11:19:41 -0400

    stuff

--- Panza.user.js
+++ Panza.user.js
@@ -30,7 +30,7 @@
 (function() {
     'use strict';
 
-    var automate=false;
+    var automate=true;
     var email_re = /(([^<>()\[\]\\.,;:\s@"：+=\/\?%]+(\.[^<>()\[\]\\.,;:：\s@"\?]+)*)|("[^\?]+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
 
     var phone_re=/[\+]?[(]?[0-9]{3}[)]?[-\s\.\/]+[0-9]{3}[-\s\.\/]+[0-9]{4,6}/im;
@@ -159,23 +159,32 @@
         var review_obj={};
         var review_pos=1;
         var last_HR=false;
-        var the_title=document.getElementsByClassName("s134yi85-0")[0].innerText;
+        var the_title="";//document.getElementsByClassName("s134yi85-0")[0].innerText;
         var comments=document.getElementsByClassName("Comment");
 
        // var pre_review_elem=comments[0].getElementsByClassName("r4x9ih-3")[0];
-        var full_review=comments[0].getElementsByClassName("r4x9ih-6")[0];
+        var full_review=comments[0].firstChild.nextElementSibling;
+        console.log("full_review.tagName1="+full_review.tagName);
+        full_review=full_review.firstChild;
+       console.log("full_review.tagName="+full_review.tagName);
+       full_review=full_review.nextElementSibling.firstChild;
+        console.log("full_review.innerText="+full_review.innerText);
         var paras=full_review.childNodes;//getElementsByClassName("s570a4-10");
         var curr_title="";
         var curr_text="";
         var curr_rating="";
-        var rating_re=/([^€\d]|^)([\d\.]+)\/([\d\.]+)/;
+        var rating_re=/([^€\d]|^)([\d\.]+)\/(10[\d\.]*)/;
         var rating_re2=/((Score)|(Rating))\s*[:\-]\s*(\d+)/;
         var blank_shit=/^\s*$/;
         var rating_match, rating_match2;
         var in_reviews=false;
         var starting_new=true,starting_new_HR=false;
 
+        var abv1=/[\d\.]+[%]\s*(ABV)?/i;
 
+            var proof_re=/([\d]{2,3}) proof/;
+            var nose_re=/(((Nose)|(Aroma))([:\s]))|(^N: )/i, color_re=/(((Colou?r)|(Appearance))([\:\s]+))|(^C\:)/i, palate_re=/(((Palate)|(Taste)|(Mouth))([:\s]+))|(^T\:)/i, finish_re=/(^Finish([\:\s]+))|(^F\:)/i;
+        var palate_re2=/(((Palate)|(Taste)|(Mouth)):([\s]*))/;
 
         var full_review2=full_review.cloneNode(true);
         paras=full_review2.childNodes;
@@ -202,14 +211,100 @@
         }
         paras=full_review.childNodes;
         console.log("full_review2.innerText="+full_review2.innerHTML);
-
+        review_obj["whiskey"+pos_parse(review_pos)+"text"]="";
         for(i=0; i < paras.length; i++)
         {
+            if(paras[i].tagName==="UL")
+            {
+                var k;
+                var the_li=paras[i].getElementsByTagName("li");
+                for(k=0; k < the_li.length; k++)
+                {
+                    if(nose_re.test(the_li[k].innerText) && review_obj["whiskey"+pos_parse(review_pos)+"nose"] === undefined)
+                    {
+                        console.log("MOO NOSE");
+                        review_obj["whiskey"+pos_parse(review_pos)+"nose"]=the_li[k].innerText.replace(/Nose([:]?)\s*/i,"");
+                    }
+                    else if(color_re.test(the_li[k].innerText) && (review_obj["whiskey"+pos_parse(review_pos)+"color"]===undefined ||
 
+                                                                  (the_li[k].innerText.toLowerCase().indexOf("color:")!==-1)))
+                    {
+                        console.log("MOO color "+the_li[k].innerText);
+                        review_obj["whiskey"+pos_parse(review_pos)+"color"]=the_li[k].innerText.replace(/((Colou?r)|(Appearance))([:\s])/i,"");
+                        console.log(review_obj["whiskey"+pos_parse(review_pos)+"color"]);
+                    }
+                    else if(palate_re.test(the_li[k].innerText) && (review_obj["whiskey"+pos_parse(review_pos)+"palate"]===undefined ||
+                                                                   (palate_re2.test(the_li[k].innerText))
+                                                                   ))
+                    {
+                        console.log("MOO palate");
+                        review_obj["whiskey"+pos_parse(review_pos)+"palate"]=the_li[k].innerText.replace(/(Palate)|(Taste)([:\s])/i,"");
+                    }
+                    else if(finish_re.test(the_li[k].innerText) && review_obj["whiskey"+pos_parse(review_pos)+"finish"]===undefined)
+                    {
+
+                        console.log("MOO finish");
+                        review_obj["whiskey"+pos_parse(review_pos)+"finish"]=the_li[k].innerText.replace(/Finish[:\s]+/i,"");
+                    }
+                    else if(in_reviews  && !/^[\d\.]+[%]?\s*(ABV)?$/i.test(paras[i].innerText)
+                       && !/^\s*[\d\.]+%\s*$/i.test(paras[i].innerText) && !/^ABV:/i.test(the_li[k].innerText))
+                    {
+                        if(review_obj["whiskey"+pos_parse(review_pos)+"text"]===undefined)
+                            review_obj["whiskey"+pos_parse(review_pos)+"text"]="";
+                        review_obj["whiskey"+pos_parse(review_pos)+"text"]=review_obj["whiskey"+pos_parse(review_pos)+"text"]+the_li[k].innerText+"\n";
+                    }
+                    if(abv1.test(the_li[k].innerText))
+                    {
+                        if(review_obj["whiskey"+pos_parse(review_pos)+"ABV"]===undefined || the_li[k].innerText.indexOf("ABV")!==-1)
+                        {
+                            var the_match=the_li[k].innerText.match(abv1)[0];
+                            var second_match_re=/([\d\.]+)/;
+                            review_obj["whiskey"+pos_parse(review_pos)+"ABV"]=Math.round(parseInt(the_match.match(second_match_re)[1]));
+                            console.log("rear whiskey="+review_obj["whiskey"+pos_parse(review_pos)+"ABV"]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(nose_re.test(paras[i].innerText) && review_obj["whiskey"+pos_parse(review_pos)+"nose"]===undefined )
+                {
+                    console.log("MOO NOSE");
+                    review_obj["whiskey"+pos_parse(review_pos)+"nose"]=paras[i].innerText.replace(/Nose:?\s*/i,"");
+                }
+                else if(color_re.test(paras[i].innerText) && (review_obj["whiskey"+pos_parse(review_pos)+"color"]===undefined ||
+                                                             paras[i].innerText.toLowerCase().indexOf("color:")!==-1)
+                                                             )
+                {
+                    console.log("MOO color");
+                    review_obj["whiskey"+pos_parse(review_pos)+"color"]=paras[i].innerText.replace(/((Colou?r)|(Appearance))[:\s]?/i,"");
+                }
+                else if(palate_re.test(paras[i].innerText) && (review_obj["whiskey"+pos_parse(review_pos)+"palate"] === undefined ||
+                                                              (palate_re2.test(paras[i].innerText))
+                                                              ))
+                {
+                    console.log("MOO palate");
+                    review_obj["whiskey"+pos_parse(review_pos)+"palate"]=paras[i].innerText.replace(/((Palate)|(Taste))[:\s]/i,"");
+                }
+                else if(finish_re.test(paras[i].innerText) && review_obj["whiskey"+pos_parse(review_pos)+"finish"]===undefined)
+                {
+
+                    console.log("MOOtoofinish");
+                    review_obj["whiskey"+pos_parse(review_pos)+"finish"]=paras[i].innerText.replace(/Finish:?\s*/i,"");
+                }
+                else if(in_reviews && !/^[\d\.]+[%]?\s*(ABV)?$/i.test(paras[i].innerText)
+                       && !/^\s*[\d\.]+%\s*$/i.test(paras[i].innerText) && !/^ABV:/i.test(paras[i].innerText))
+                {
+                    if(review_obj["whiskey"+pos_parse(review_pos)+"text"]===undefined) {
+                        review_obj["whiskey"+pos_parse(review_pos)+"text"]=""; }
+                    review_obj["whiskey"+pos_parse(review_pos)+"text"]=review_obj["whiskey"+pos_parse(review_pos)+"text"]+paras[i].innerText+"\n";
+                }
+            }
 
             if((paras[i].getElementsByTagName("h4")!==undefined && paras[i].getElementsByTagName("h4").length>0) ||
               (paras[i].getElementsByTagName("strong")!==undefined && paras[i].getElementsByTagName("strong").length>0) || (paras[i].tagName==="H4")
-               || (paras[i].tagName==="H3") 
+               || (paras[i].tagName==="H3") ||
+               (paras[i].tagName==="P" && /Review(\s[\d]+)?:/.test(paras[i].innerText))
               )
             {
                 console.log("Enteringreviews");
@@ -221,16 +316,20 @@
             {
                 last_HR=true;
                 console.log("\n\nfound HR\n\n");
-                if(!(curr_title.length===0 && curr_rating.length===0 && curr_text.length===0))
+                if(!(review_obj["whiskey"+pos_parse(review_pos)+"palate"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"color"]===undefined &&
+                     review_obj["whiskey"+pos_parse(review_pos)+"nose"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"finish"]===undefined
+                    ))
                 {
-                    review_obj["WhiskeyName"+review_pos]=curr_title;
-                    review_obj["Rating"+review_pos]=curr_rating;
-                    review_obj["ReviewText"+review_pos]=curr_text;
+                   // review_obj["WhiskeyName"+review_pos]=curr_title;
+                   // review_obj["Rating"+review_pos]=curr_rating;
+                   // review_obj["ReviewText"+review_pos]=curr_text;
 
                     /* Last one */
                     starting_new=true;
                     starting_new_HR=true;
-                    //review_pos=review_pos+1;
+                    if(review_obj["whiskey"+pos_parse(review_pos)+"text"]===undefined ||
+                       review_obj["whiskey"+pos_parse(review_pos)+"text"].length===0) review_obj["whiskey"+pos_parse(review_pos)+"text"]="NA";
+                    review_pos=review_pos+1;
                     curr_title="";
                     curr_text="";
                     curr_rating="";
@@ -266,17 +365,23 @@
 
                 }
 
-                if(!(curr_title.length===0 && curr_rating.length===0 && curr_text.length===0))
+                if(!(review_obj["whiskey"+pos_parse(review_pos)+"palate"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"color"]===undefined &&
+                     review_obj["whiskey"+pos_parse(review_pos)+"nose"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"finish"]===undefined
+                    ))
                 {
-                    review_obj["WhiskeyName"+review_pos]=curr_title;
-                    review_obj["Rating"+review_pos]=curr_rating;
-                    review_obj["ReviewText"+review_pos]=curr_text;
+                    //review_obj["WhiskeyName"+review_pos]=curr_title;
+                    //review_obj["Rating"+review_pos]=curr_rating;
+                  //  review_obj["whiskey"+review_pos+"text"]=curr_text;
 
 
                     /* Last one */
-                    starting_new=true;
+                  starting_new=true;
                     starting_new_HR=false;
+                    if(review_obj["whiskey"+pos_parse(review_pos)+"text"]===undefined ||
+                       review_obj["whiskey"+pos_parse(review_pos)+"text"].length===0) review_obj["whiskey"+pos_parse(review_pos)+"text"]="NA";
+
                     review_pos=review_pos+1;
+                  //  if(review_pos>1) break;
                     console.log("\treview_pos="+review_pos);
 
                     curr_title="";
@@ -287,12 +392,14 @@
                 {
                     starting_new=true;
                     starting_new_HR=false;
+                    //review_pos=review_pos+1;
                 }
             }
             else if(in_reviews && starting_new &&
                     ( (paras[i].getElementsByTagName("h4")!==undefined && paras[i].getElementsByTagName("h4").length>0) ||
               (paras[i].getElementsByTagName("strong")!==undefined && paras[i].getElementsByTagName("strong").length>0) ||
-                     (paras[i].tagName==="H4") || paras[i].tagName==="H3" || paras[i].tagName==="A" 
+                     (paras[i].tagName==="H4") || paras[i].tagName==="H3" || paras[i].tagName==="A" ||
+                     (paras[i].getElementsByTagName("em")!==undefined && paras[i].getElementsByTagName("em").length>0)
                      
               ))
             {
@@ -316,11 +423,48 @@
                 starting_new_HR=false;
             }
 
-            else if(in_reviews)
+
+
+            console.log("MOOSECPW");
+
+
+            if(abv1.test(paras[i].innerText))
             {
-                curr_text=curr_text+paras[i].innerText+"\n";
+                if(review_obj["whiskey"+pos_parse(review_pos)+"ABV"]===undefined || paras[i].innerText.indexOf("ABV")!==-1)
+                {
+                    var the_match2=paras[i].innerText.match(abv1)[0];
+                    console.log("the_match2="+JSON.stringify(the_match2));
+                    var second_match_re2=/([\d\.]+)[^\d\.]/;
+                    var the_val=Math.round(parseFloat(the_match2.replace(/%.*$/,"")));
+                    if(the_val < 100)
+                    {
+                        review_obj["whiskey"+pos_parse(review_pos)+"ABV"]=the_val;
+                    }
+                    console.log("Here whiskey="+review_obj["whiskey"+pos_parse(review_pos)+"ABV"]);
+                }
             }
+            if(proof_re.test(paras[i].innerText) && review_obj["ABV"+review_pos]===undefined)
+            {
+                var proof_match=paras[i].innerText.match(proof_re)[1];
+                console.log("proof_match="+JSON.stringify(proof_match)+"****\n*****\n");
+
+                review_obj["whiskey"+pos_parse(review_pos)+"ABV"]=parseInt(proof_match/2);
+            }
+
+
         }
+        if((review_obj["whiskey"+pos_parse(review_pos)+"palate"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"color"]===undefined &&
+                     review_obj["whiskey"+pos_parse(review_pos)+"nose"]===undefined && review_obj["whiskey"+pos_parse(review_pos)+"finish"]===undefined
+                    && review_obj["whiskey"+pos_parse(review_pos)+"text"]!==undefined)&&review_pos>1)
+        {
+            review_obj["whiskey"+pos_parse(review_pos-1)+"text"]= review_obj["whiskey"+pos_parse(review_pos-1)+"text"]+
+                "\n"+review_obj["whiskey"+pos_parse(review_pos)+"text"];
+                            delete review_obj["whiskey"+pos_parse(review_pos)+"text"];
+
+        }
+
+
+
         console.log("review_obj="+JSON.stringify(review_obj));
         GM_setValue("review_obj",JSON.stringify(review_obj));
 
@@ -332,17 +476,30 @@
 
 
 
+    function pos_parse(val)
+    {
+        if(val < 10) return "0"+val;
+        return val;
+    }
 
     function init_Query()
     {
 
        //var wT=document.getElementById("workContent").getElementsByTagName("table")[0];
-        var mForm=document.getElementById("mturk_form");
-        var inner_a=mForm.getElementsByTagName("a")[4];
+        var mForm=document.getElementsByClassName("row")[0].getElementsByTagName("section")[0];
+        var inner_a=mForm.getElementsByTagName("a")[0];
         console.log("inner_a.innerText="+inner_a.innerText);
         my_query={url: inner_a.href};
 
-        my_query.url=my_query.url.replace(/\/[a-z][a-z]\.reddit\.com/,"/www.reddit.com")
+        my_query.url=my_query.url.replace(/\/[a-z][a-z]\.reddit\.com/,"/www.reddit.com");
+        if(document.getElementsByName("whiskey02text").length>0 &&
+          document.getElementsByName("whiskey02text")[0].type!==undefined &&
+          document.getElementsByName("whiskey02text")[0].type!=="hidden")
+        {
+            console.log("Too many reviews");
+            GM_setValue("returnHit",true);
+            return;
+        }
          console.log("my_query.url="+my_query.url);
         GM_setValue("review_obj","");
         GM_setValue("url", my_query.url+"?sort=old");
@@ -352,8 +509,18 @@
        GM_addValueChangeListener("review_obj", function() {
            var review_obj=JSON.parse(GM_getValue("review_obj"));
            //console.log("review_obj="+JSON.stringify(review_obj));
+           var review_pos=1;
+           var curr_select;
+           while((curr_select=document.getElementById("whiskey"+pos_parse(review_pos)+"name"))!==null)
+           {
+               console.log("Setting value for "+review_pos);
+               curr_select.value=curr_select.options[review_pos].value;
+               review_pos++;
+           }
+
            var x;
            var ct_stuff=0;
+
            for(x in review_obj)
            {
                ct_stuff=ct_stuff+1;
@@ -406,7 +573,7 @@
         /* Do reddit */
         GM_setValue("url","");
         GM_addValueChangeListener("url",function() {
-            window.location.href=GM_getValue("url"); });
+            window.location.href=GM_getValue("url").replace(/http:/,"https:"); });
         /* Start parsing review */
         parse_reddit();
     }
