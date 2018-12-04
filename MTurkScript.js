f1695bc9a09dae74a2af0d7185a272a394777db5
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-12-03 20:34:41 -0500

    stuff

--- MTurkScript.js
+++ MTurkScript.js
@@ -467,7 +467,7 @@ MTurkScript.prototype.shorten_company_name=function(name)
     name=name.replace(/\s+Pte$/i,"").replace(/ AG$/i,"");
     name=name.replace(/\s+S\.?A\.?$/i,"").replace(/\s+L\.?P\.?$/i,"");
     name=name.replace(/\s+GmbH$/i,"").replace(/\s+SRL/i,"")
-    name=name.replace(/\s+Sarl$/i,"");
+    name=name.replace(/\s+Sarl$/i,"").replace(/\s+KG/i,"");
     name=name.replace(/[,\.]+$/,"");
     name=name.replace(/\sCo\.?$/i,"");
 
