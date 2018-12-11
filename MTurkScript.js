2a0f2524b841b82b5e833ba0b10890502dbf7b6a
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2018-12-11 14:34:42 -0500

    stuff

--- MTurkScript.js
+++ MTurkScript.js
@@ -454,7 +454,13 @@ MTurkScript.prototype.parse_name=function(to_parse)
 
 MTurkScript.prototype.shorten_company_name=function(name)
 {
-    name=removeDiacritics(name).replace(/ - .*$/,"").trim().replace(/\s*plc$/i,"");
+    var first_suffix_str="(Pty Ltd(\\.)?)|Limited|LLC(\\.?)";
+    var first_regex=new RegExp("\\s*"+first_suffix_str+"$","i");
+    const reducer = (accumulator, currentValue) => accumulator.replace(currentValue,"").trim();
+    
+    name=removeDiacritics(name);
+    name=name.replace(first_regex);
+    name=name.replace(/ - .*$/,"").trim().replace(/\s*plc$/i,"");
     name=name.replace(/\(.*$/i,"").trim();
     name=name.replace(/\s*Corporation$/i,"").replace(/\s*Corp\.?$/i,"");
     name=name.replace(/\s*Incorporated$/i,"").replace(/\s*Inc\.?$/i,"");
