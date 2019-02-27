7783ae422ec35b67d5892a06cafbf2639cab5e15
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-02-27 13:37:59 -0500

    stuff

--- js/MTurkScript.js
+++ js/MTurkScript.js
@@ -1315,7 +1315,8 @@ MTurkScript.prototype.fix_addy_script_only=function(script) {
 	    else script.innerHTML=email;
 //            console.log("script.parentNode="+script.parentNode);
         }
-    };
+};
+
 
 /* Fixes the hidden emails in a document */
 
@@ -1346,6 +1347,7 @@ MTurkScript.prototype.fix_emails=function(doc,url) {
         //console.log("scripts["+x+"]="+scripts[x].innerHTML);
         var match=scripts[x].innerHTML.match(unesc_regex),decoded,match2;
         if(/var addy[\d]+/.test(scripts[x].innerHTML)) MTurkScript.prototype.fix_addy_script_only(scripts[x]);
+	if/function escramble/.test(scripts[x].innerHTML)) MTurkScript.prototype.fix_escramble(scripts[x]);
         if(match&&(decoded=decodeURIComponent(match[1]))&&(match2=decoded.match(email_re)) && my_query.fields.email.length===0) {
             console.log("Matched weird decode");
 	    
