f54260a0f7b1f3954387e6c0484679159f52b340
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-03-28 11:33:04 -0400

    stuff

--- js/MTurkScript.js
+++ js/MTurkScript.js
@@ -773,7 +773,7 @@ MTurkScript.prototype.parse_FB_about=function(doc,url,resolve,reject) {
     }
     
     for(i=0; i < code.length; i++) code[i].innerHTML=code[i].innerHTML.replace(/^<!-- /,"").replace(/-->$/,"");
-    if((closed=doc.querySelector("._14-5"))) result.is_closed=closed.innerText;
+    if((closed=doc.querySelector("._14-5")) && /Permanently Closed/i.test(closed.innerText)) result.is_closed=true;
     var about_fields=doc.getElementsByClassName("_3-8j"),inner_field1,text;
     var _a3f=doc.getElementsByClassName("_a3f"),coord_ret; // map with coords
     if(_a3f.length>0 && (coord_ret=MTurkScript.prototype.FB_match_coords(_a3f[0].src))) {
