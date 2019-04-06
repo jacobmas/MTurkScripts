e6bd4c7a98c7d1d4cdb20c27fc8bc28c1f3b6fea
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-04-05 23:07:49 -0400

    update granicus

--- Govt/Government.js
+++ Govt/Government.js
@@ -1064,10 +1064,12 @@ Gov.get_granicus_staff_directory=function(doc,url,resolve,reject,response) {
 	    dept=parsed[i].SubGroupName||"";
 	    if(!parsed[i].contacts) continue;
 	    for(j=0;j<parsed[i].contacts.length;j++) {
-		curr_elem=parsed[i].contacts[j];
-		curr_person={name:curr_elem.name||"",title:curr_elem.title||"",phone:curr_elem.phone||"",
+	/*	curr_elem=parsed[i].contacts[j];
+		curr_person={name:curr_elem.name||"",
+			     title:curr_elem.title||"",
+			     phone:curr_elem.phone||"",
 			     email:curr_elem.Emails?curr_elem.Emails.replace(/,.*$/,"")||"",department:dept};
-		Gov.contact_list.push(curr_person);
+		Gov.contact_list.push(curr_person);*/
 	    }
 	}
 		
