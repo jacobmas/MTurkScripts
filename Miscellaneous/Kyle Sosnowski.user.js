b235bbdfd6a5c9d701d226bc9a097f039d5824cf
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-08-15 13:23:26 -0400

    stuff

---
+++ Miscellaneous/Kyle Sosnowski.user.js
@@ -0,0 +1,171 @@
+// ==UserScript==
+// @name         Kyle Sosnowski
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  New script
+// @author       You
+// @include        http://*.mturkcontent.com/*
+// @include        https://*.mturkcontent.com/*
+// @include        http://*.amazonaws.com/*
+// @include        https://*.amazonaws.com/*
+// @include https://worker.mturk.com/*
+// @grant  GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @grant GM_addValueChangeListener
+// @grant GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @grant GM_openInTab
+// @grant GM_getResourceText
+// @grant GM_addStyle
+// @connect google.com
+// @connect bing.com
+// @connect yellowpages.com
+// @connect *
+// @require https://raw.githubusercontent.com/hassansin/parse-address/master/parse-address.min.js
+// @require https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/js/MTurkScript.js
+// @resource GlobalCSS https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/global/globalcss.css
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var my_query = {};
+    var bad_urls=[];
+    var MTurk=new MTurkScript(20000,750+(Math.random()*1000),[],begin_script,"A37A3XMFDEUPRN",true);
+    var MTP=MTurkScript.prototype;
+    function is_bad_name(b_name)
+    {
+        return false;
+    }
+
+    function query_response(response,resolve,reject,type) {
+        var doc = new DOMParser()
+        .parseFromString(response.responseText, "text/html");
+        console.log("in query_response\n"+response.finalUrl);
+        var search, b_algo, i=0, inner_a;
+        var b_url="crunchbase.com", b_name, b_factrow,lgb_info, b_caption,p_caption;
+        var b1_success=false, b_header_search,b_context,parsed_context,parsed_lgb;
+        try
+        {
+            search=doc.getElementById("b_content");
+            b_algo=search.getElementsByClassName("b_algo");
+            lgb_info=doc.getElementById("lgb_info");
+            b_context=doc.getElementById("b_context");
+            console.log("b_algo.length="+b_algo.length);
+	    if(b_context&&(parsed_context=MTP.parse_b_context(b_context))) {
+                console.log("parsed_context="+JSON.stringify(parsed_context)); } 
+            if(lgb_info&&(parsed_lgb=MTP.parse_lgb_info(lgb_info))) {
+                    console.log("parsed_lgb="+JSON.stringify(parsed_lgb)); }
+            for(i=0; i < b_algo.length; i++) {
+                b_name=b_algo[i].getElementsByTagName("a")[0].textContent;
+                b_url=b_algo[i].getElementsByTagName("a")[0].href;
+                b_caption=b_algo[i].getElementsByClassName("b_caption");
+                p_caption=(b_caption.length>0 && b_caption[0].getElementsByTagName("p").length>0) ?
+                    p_caption=b_caption[0].getElementsByTagName("p")[0].innerText : '';
+                console.log("("+i+"), b_name="+b_name+", b_url="+b_url+", p_caption="+p_caption);
+                if(!MTurkScript.prototype.is_bad_url(b_url, bad_urls) && !is_bad_name(b_name) && (b1_success=true)) break;
+            }
+            if(b1_success && (resolve(b_url)||true)) return;
+        }
+        catch(error) {
+            reject(error);
+            return;
+        }
+        reject("Nothing found");
+        return;
+    }
+
+    /* Search on bing for search_str, parse bing response with callback */
+    function query_search(search_str, resolve,reject, callback,type,filters) {
+        console.log("Searching with bing for "+search_str);
+        if(!filters) filters="";
+        var search_URIBing='https://www.bing.com/search?q='+
+            encodeURIComponent(search_str)+"&filters="+filters+"&first=1&rdr=1";
+        GM_xmlhttpRequest({method: 'GET', url: search_URIBing,
+                           onload: function(response) { callback(response, resolve, reject,type); },
+                           onerror: function(response) { reject("Fail"); },ontimeout: function(response) { reject("Fail"); }
+                          });
+    }
+
+    /* Following the finding the district stuff */
+    function query_promise_then(result) {
+        submit_if_done();
+    }
+
+    function begin_script(timeout,total_time,callback) {
+        if(timeout===undefined) timeout=200;
+        if(total_time===undefined) total_time=0; 
+        if(callback===undefined) callback=init_Query;
+        if(MTurk!==undefined) { callback(); }
+        else if(total_time<2000) {
+            console.log("total_time="+total_time);
+            total_time+=timeout;
+            setTimeout(function() { begin_script(timeout,total_time,callback); },timeout);
+            return;
+        }
+        else { console.log("Failed to begin script"); }
+    }
+
+    function add_to_sheet() {
+        var x,field;
+        for(x in my_query.fields) {
+            if((MTurk.is_crowd && (field=document.getElementsByName(x)[0])) ||
+               (!MTurk.is_crowd && (field=document.getElementById(x)))) field.value=my_query.fields[x];
+        }
+    }
+
+    function submit_if_done() {
+        var is_done=true,x;
+        add_to_sheet();
+        for(x in my_query.done) if(!my_query.done[x]) is_done=false;
+        if(is_done && !my_query.submitted && (my_query.submitted=true)) MTurk.check_and_submit();
+    }
+    function parse_url1(doc,url,resolve,reject) {
+        var link=doc.querySelector(".item-list__link");
+        console.log("parse_url1,url="+url);
+        if(!link) {
+            my_query.fields.upc="na";
+            resolve("");
+            return;
+        }
+        link.href=MTP.fix_remote_url(link.href,url);
+        var promise=MTP.create_promise(link.href,parse_url2,resolve,reject);
+
+    }
+    function parse_url2(doc,url,resolve,reject) {
+        console.log("parse_url2,url="+url);
+        var upc_code;
+        var upc_code_lst=doc.querySelectorAll(".item-overview__meta-item");
+        var t=0;
+        while(t<upc_code_lst.length && upc_code_lst[t].querySelector(".star-rating")) {
+            t++;
+        }
+        upc_code=t<upc_code_lst.length?upc_code_lst[t]:null;
+        if(upc_code) {
+            console.log("upc_code.innerText="+upc_code.innerText);
+            var re=/UPC:\s*([\d]+)/,match;
+            match=upc_code.innerText.match(re);
+            if(!match) my_query.fields.upc="na";
+            else my_query.fields.upc=match[1];
+            resolve("");
+            return;
+        }
+        else {
+            my_query.fields.upc="na";
+            resolve("");
+            return;
+        }
+
+    }
+    function init_Query()
+    {
+        console.log("in init_query");
+        var i;
+        
+        my_query={url:document.querySelector("form a").href,fields:{},done:{},submitted:false};
+	console.log("my_query="+JSON.stringify(my_query));
+        var promise=MTP.create_promise(my_query.url,parse_url1,query_promise_then);
+    //item-list__link
+    }
+
+})();
---
+++ Miscellaneous/Tomas Lobo.user.js
@@ -0,0 +1,170 @@
+// ==UserScript==
+// @name         Tomas Lobo
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  New script
+// @author       You
+// @include        http://*.mturkcontent.com/*
+// @include        https://*.mturkcontent.com/*
+// @include        http://*.amazonaws.com/*
+// @include        https://*.amazonaws.com/*
+// @include https://worker.mturk.com/*
+// @grant  GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @grant GM_addValueChangeListener
+// @grant GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @grant GM_openInTab
+// @grant GM_getResourceText
+// @grant GM_addStyle
+// @connect google.com
+// @connect bing.com
+// @connect yellowpages.com
+// @connect *
+// @require https://raw.githubusercontent.com/hassansin/parse-address/master/parse-address.min.js
+// @require https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/js/MTurkScript.js
+// @resource GlobalCSS https://raw.githubusercontent.com/jacobmas/MTurkScripts/master/global/globalcss.css
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var my_query = {};
+    var bad_urls=[];
+    var MTurk=new MTurkScript(20000,750+(Math.random()*1000),[],begin_script,"A4PXU10RT5F5B",false);
+    var MTP=MTurkScript.prototype;
+    function is_bad_name(b_name)
+    {
+        return false;
+    }
+
+    function query_response(response,resolve,reject,type) {
+        var doc = new DOMParser()
+        .parseFromString(response.responseText, "text/html");
+        console.log("in query_response\n"+response.finalUrl);
+        var search, b_algo, i=0, inner_a;
+        var b_url="crunchbase.com", b_name, b_factrow,lgb_info, b_caption,p_caption;
+        var b1_success=false, b_header_search,b_context,parsed_context,parsed_lgb;
+        try
+        {
+            search=doc.getElementById("b_content");
+            b_algo=search.getElementsByClassName("b_algo");
+            lgb_info=doc.getElementById("lgb_info");
+            b_context=doc.getElementById("b_context");
+            console.log("b_algo.length="+b_algo.length);
+	    if(b_context&&(parsed_context=MTP.parse_b_context(b_context))) {
+                console.log("parsed_context="+JSON.stringify(parsed_context)); } 
+            if(lgb_info&&(parsed_lgb=MTP.parse_lgb_info(lgb_info))) {
+                    console.log("parsed_lgb="+JSON.stringify(parsed_lgb)); }
+            for(i=0; i < b_algo.length; i++) {
+                b_name=b_algo[i].getElementsByTagName("a")[0].textContent;
+                b_url=b_algo[i].getElementsByTagName("a")[0].href;
+                b_caption=b_algo[i].getElementsByClassName("b_caption");
+                p_caption=(b_caption.length>0 && b_caption[0].getElementsByTagName("p").length>0) ?
+                    p_caption=b_caption[0].getElementsByTagName("p")[0].innerText : '';
+                console.log("("+i+"), b_name="+b_name+", b_url="+b_url+", p_caption="+p_caption);
+                if(!MTurkScript.prototype.is_bad_url(b_url, bad_urls) && !is_bad_name(b_name) && (b1_success=true)) break;
+            }
+            if(b1_success && (resolve(b_url)||true)) return;
+        }
+        catch(error) {
+            reject(error);
+            return;
+        }
+        reject("Nothing found");
+        return;
+    }
+
+    /* Search on bing for search_str, parse bing response with callback */
+    function query_search(search_str, resolve,reject, callback,type,filters) {
+        console.log("Searching with bing for "+search_str);
+        if(!filters) filters="";
+        var search_URIBing='https://www.bing.com/search?q='+
+            encodeURIComponent(search_str)+"&filters="+filters+"&first=1&rdr=1";
+        GM_xmlhttpRequest({method: 'GET', url: search_URIBing,
+                           onload: function(response) { callback(response, resolve, reject,type); },
+                           onerror: function(response) { reject("Fail"); },ontimeout: function(response) { reject("Fail"); }
+                          });
+    }
+
+    /* Following the finding the district stuff */
+    function query_promise_then(result) {
+    }
+
+    function begin_script(timeout,total_time,callback) {
+        if(timeout===undefined) timeout=200;
+        if(total_time===undefined) total_time=0; 
+        if(callback===undefined) callback=init_Query;
+        if(MTurk!==undefined) { callback(); }
+        else if(total_time<2000) {
+            console.log("total_time="+total_time);
+            total_time+=timeout;
+            setTimeout(function() { begin_script(timeout,total_time,callback); },timeout);
+            return;
+        }
+        else { console.log("Failed to begin script"); }
+    }
+
+    function add_to_sheet() {
+        var x,field;
+        for(x in my_query.fields) {
+            if((MTurk.is_crowd && (field=document.getElementsByName(x)[0])) ||
+               (!MTurk.is_crowd && (field=document.getElementById(x)))) field.value=my_query.fields[x];
+        }
+    }
+
+    function submit_if_done() {
+        var is_done=true,x;
+        add_to_sheet();
+        for(x in my_query.done) if(!my_query.done[x]) is_done=false;
+        if(is_done && !my_query.submitted && (my_query.submitted=true)) MTurk.check_and_submit();
+    }
+
+    function do_generic_query(resolve,reject) {
+        var headers={"Content-Type":"application/x-www-form-urlencoded","host":"apps.fomento.gob.es",
+                     "origin":"https://apps.fomento.gob.es","referer":"https://apps.fomento.gob.es/crgt/servlet/ServletController",
+                     "Upgrade-Insecure-Requests": "1"};
+        var data={
+            "consulta": my_query.name,
+            "modulo": "datosconsulta",
+            "tpsolic": "E",
+            "accion": "consultar_empre",
+            "nocaptcha": "no",
+        "btnOk": "Consultar"};
+        var data_str=MTP.json_to_post(data).replace(/%20/g,"+");
+        console.log("data_str="+data_str);
+        var query_url="https://apps.fomento.gob.es/crgt/servlet/ServletController";
+        GM_xmlhttpRequest({method: 'POST', url: query_url,data:data_str,headers:headers,
+                           onload: function(response) {
+                               var doc = new DOMParser().parseFromString(response.responseText, "text/html");
+                               parse_spanishpage(doc,query_url,resolve,reject); },
+                           onerror: function(response) { console.log("Fail"); },ontimeout: function(response) { console.log("Fail"); }
+                          });
+    }
+    function parse_spanishpage(doc,url,resolve,reject) {
+        console.log("parse_spanishpage,url="+url);
+        var ficha2=doc.querySelector(".ficha2");
+        console.log("ficha2="+ficha2);
+        if(ficha2) console.log(ficha2.innerText);
+        else console.log(doc.body.innerHTML);
+    }
+
+    function init_Query()
+    {
+        console.log("in init_query");
+        var i;
+        var wT=document.getElementById("DataCollection").getElementsByTagName("table")[0];
+        //var dont=document.getElementsByClassName("dont-break-out");
+        var url="https://apps.fomento.gob.es/crgt/servlet/ServletController?modulo=datosconsulta&accion=inicio&lang=es&estilo=default";
+        my_query={name:wT.rows[1].cells[1].innerText,fields:{},done:{},submitted:false};
+	console.log("my_query="+JSON.stringify(my_query));
+        var search_str;
+        const queryPromise = new Promise((resolve, reject) => {
+            do_generic_query(resolve,reject);
+        });
+        queryPromise.then(query_promise_then)
+            .catch(function(val) {
+            console.log("Failed at this queryPromise " + val); GM_setValue("returnHit"+MTurk.assignment_id,true); });
+    }
+
+})();
