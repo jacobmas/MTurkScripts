96e8b1b6d773b246b3c60a1a7fe67da273944484
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-09-06 14:53:33 -0400

    gov updates

--- Govt/Government.js
+++ Govt/Government.js
@@ -405,7 +405,9 @@ Gov.parse_contact_elems=function(doc,url,resolve,reject,name) {
     var my_count=0;
     div.forEach(function(elem) {
 	/* Skip if there is exactly 1 nested child */
+	if(elem.children.length===1) console.log("elem.children[0]="+elem.children[0].tagName);
 	if(elem.children.length===1 && /^(P|DIV|SECTION|TD|LI)$/.test(elem.children[0].tagName)) return;
+	
 	my_count++;
 	add_count+=Gov.parse_contact_div(elem,name,url); });
     console.log("divs visited="+my_count);
