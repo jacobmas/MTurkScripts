336ea869af361dde8e5c232fcc277d161070a045
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-09-19 13:14:59 -0400

    stuff

--- Email/MailTester.js
+++ Email/MailTester.js
@@ -194,7 +194,7 @@ MailTester.prototype.mailtester_response=function(doc,url,resolve,reject,email,s
         let lastRow=table.rows[table.rows.length-1];
         let lastCell=lastRow.cells[lastRow.cells.length-1];
         let cellText=lastCell.innerText;
-        console.log("email="+email+", lastCell="+lastCell.innerHTML);
+        console.log("email="+email);//+", lastCell="+lastCell.innerHTML);
         if(cellText.indexOf("E-mail address is valid")!==-1||
            cellText.indexOf("The user you are trying to contact is receiving mail at a rate that")!==-1) {
             this.email_list.push(new EmailQual(email,url,this.fullname,this.domain));
