2d7a67622f597ff5f59d12a4d8b8e12cb169925c
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-09-30 18:22:07 -0400

    stuff

--- js/MTurkScript.js
+++ js/MTurkScript.js
@@ -1375,7 +1375,7 @@ MTurkScript.prototype.fix_emails=function(doc,url) {
     for(i=0; i < links.length; i++) {
         //console.log("("+i+"): "+links[i].href+", "+links[i].innerText);
 	if((local=links[i].querySelector(".localMail")) && (domain=links[i].querySelector(".domainMail"))) {
-	    links[i].href=links[i].innerText="mailto:"+local.innerText.trim+"@"+domain.innerText.trim();
+	    links[i].href=links[i].innerText="mailto:"+local.innerText.trim()+"@"+domain.innerText.trim();
 	}
         if(links[i].dataset.encEmail && (temp_email=MTurkScript.prototype.swrot13(links[i].dataset.encEmail.replace(/\[at\]/,"@")))
            && !MTurkScript.prototype.is_bad_email(temp_email)) links[i].href="mailto:"+temp_email;
