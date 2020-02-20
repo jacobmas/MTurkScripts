81ad14c75acad6029a43ff1c21cc8dc5fb0afd87
Jacob Alperin-Sheriff <jacobmas@gmail.com>
2020-02-20 13:53:22 -0500

    update script

--- js/MTurkScript.js
+++ js/MTurkScript.js
@@ -264,7 +264,7 @@ MTurkScript.prototype.begin_crowd_script=function(timeout,total_time,callback,se
     let assignmentId=window.location.href.match(/assignmentId\=([A-Z0-9]*)/);
     if(assignmentId) this.assignment_id=assignmentId[1];
     else { console.log("No assignmentId found"); }
-    if((document.querySelector("crowd-button") && !document.querySelector("crowd-button").disabled)) {
+    if((document.querySelector("crowd-button") && !document.querySelector("crowd-button").disabled) || self.is_crowd_ready()) {
 	self.submit_button=document.querySelector("crowd-button");
 	console.log("self.submit_button="+self.submit_button);
 	console.log(self);
