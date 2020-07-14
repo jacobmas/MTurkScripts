90ff07d1f95f6af36a671617f10020c36cb86f83
Jacob Alperin-Sheriff <jacobmas@gmail.com>
2020-07-14 08:57:01 -0400

    stuff

--- Govt/Government.js
+++ Govt/Government.js
@@ -843,6 +843,7 @@ Gov.initial_cleanup_text_for_parse=function(text) {
 };
 /* Basic check of bad ret, can be improved */
 Gov.parsed_ret_is_bad=function(ret) {
+    if(Gov.debug) console.log("Gov.parsed_ret_is_bad,ret="+JSON.stringify(ret));
     if(ret && ret.name && /\s+for\s+/.test(ret.name)) return true;
     if(ret&&ret.title&& /[\d]{2,}/.test(ret.title)) return true;
     return false;
