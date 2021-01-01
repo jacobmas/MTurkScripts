b172372515d52bd5193b584ad265e379f546cbe6
Jacob Alperin-Sheriff <jacobmas@gmail.com>
2020-12-31 20:42:55 -0500

    new email

--- js/MTurkScript.js
+++ js/MTurkScript.js
@@ -1396,6 +1396,16 @@ MTurkScript.prototype.fix_emails_in_scripts=function(doc,url,the_script) {
 MTurkScript.prototype.fix_emails=function(doc,url) {
     var i,links=doc.links,j,script,scripts=doc.scripts;
     var my_match,temp_email,encoded_match,match_split,clicky,local,domain;
+	
+	doc.querySelectorAll(".emailli").forEach(function(elem) {
+		let a;
+		if(elem.dataset.l && elem.dataset.r && (a=elem.querySelector("a"))) {
+			a.href="mailto:"+elem.dataset.l+"@"+elem.dataset.r.replace(/%/g,".");
+			a.innerHTML=elem.dataset.l+"@"+elem.dataset.r.replace(/%/g,".");
+		}
+		
+	});
+	
     for(i=0; i < links.length; i++) {
         //console.log("("+i+"): "+links[i].href+", "+links[i].innerText);
 	if((local=links[i].querySelector(".localMail")) && (domain=links[i].querySelector(".domainMail"))) {
